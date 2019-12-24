217824aef319b1a1bc95ef32af35a8fcac155f72
ytyaru <ytyaru@outlook.jp>
2019-12-24 11:34:40 +0900

    修正するも永久待機する

--- src/StdIn.cs
+++ src/StdIn.cs
@@ -1,5 +1,7 @@
 using System;
+using System.IO;
 class StdIn {
+    /*
     public static void Main(string[] args){
 //        var stdin = System.Console.Read();
 //        System.Console.WriteLine(stdin);
@@ -8,6 +10,17 @@ class StdIn {
         while ((s = Console.ReadLine()) != null)
         {
             Console.WriteLine(s);
+        }
+
+    }
+    */
+    static void Main(string[] args)
+    {
+        Console.SetIn(new StreamReader(Console.OpenStandardInput(8192))); // This will allow input >256 chars
+        while (Console.In.Peek() != -1)
+        {
+            string input = Console.In.ReadLine();
+            Console.WriteLine("Data read was " + input);
         }
     }
 }
--- src/run.sh
+++ src/run.sh
@@ -4,5 +4,7 @@ Run() {
 	csc -nologo "./StdIn.cs"
 	chmod +x "./StdIn.exe"
 	echo -e "A\n\tB" | "./StdIn.exe"
+# 永久待機……
+	"./StdIn.exe"
 }
 Run "$@"
